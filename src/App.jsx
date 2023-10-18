6b8db6500839e93602aa05e016cc7575decb69dd
MOH FAIZAN <faizanusmani06@gmail.com>
2023-10-19 04:02:19 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,103 @@
+// import Hooks from react
+import { useState, useCallback, useEffect, useRef } from "react";
+
+// App component
+function App() {
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  // using refhook
+  const passwordRef = useRef(null);
+
+  // PasswordGenerator
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    if (numberAllowed) str += "0123456789";
+    if (charAllowed) str += "~`!@#$%^&*_-+={}[]";
+
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
+    }
+    // update password
+    setPassword(pass);
+  }, [length, numberAllowed, charAllowed, setPassword]);
+
+  // Copy to clipboard
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0, 999);
+    window.navigator.clipboard.writeText(password)
+  }, [password])
+
+  // useEffect Hook
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, charAllowed, numberAllowed, passwordGenerator]);
+
+  // JSX
+  return (
+    <div className="w-full max-w-xl h-fit mx-auto shadow-md rounded-3xl px-10 py-7 my-8 bg-gray-800 text-orange-500 text-3xl">
+      <h1 className="text-[#05fcba] text-center my-7">Password Generator</h1>
+      <div className="flex shadow rounded-lg overflow-hidden mb-4">
+        <input
+          type="text"
+          value={password}
+          className="outline-none w-full py-1 px-3 text-xl bg-[#252626] text-white"
+          placeholder="Password"
+          readOnly
+          ref={passwordRef}
+        />
+        <button
+          onClick={copyPasswordToClipboard}
+          className="outline-none bg-blue-800 text-white px-3 py-2 shrink-0 hover:bg-blue-700 duration-200"
+        >
+          Copy
+        </button>
+      </div>
+      <div className="flex justify-between text-xl">
+        <div className="flex items-center gap-x-1">
+          <input
+            type="range"
+            min={6}
+            max={100}
+            value={length}
+            className="cursor-pointer"
+            onChange={(e) => {
+              setLength(e.target.value);
+            }}
+          />
+          <label>Length: {length}</label>
+        </div>
+        <div className="flex items-center gap-x-1">
+          <input
+            type="checkbox"
+            defaultChecked={numberAllowed}
+            id="numberInput"
+            onChange={() => {
+              setNumberAllowed((prev) => !prev);
+            }}
+          />
+          <label htmlFor="numberInput">Numbers</label>
+        </div>
+        <div className="flex items-center gap-x-1">
+          <input
+            type="checkbox"
+            defaultChecked={charAllowed}
+            id="characterInput"
+            onChange={() => {
+              setCharAllowed((prev) => !prev);
+            }}
+          />
+          <label htmlFor="characterInput">Characters</label>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
